6ad31e0226ef2fbf09184bab9fd674bf0c66cc4c
Alban Mouton <alban.mouton@gmail.com>
2018-05-16 14:51:54 +0200

    replace config-store with application-router

--- express/application-router.js
+++ express/application-router.js
@@ -12,17 +12,26 @@ router.use(bodyParser.json({limit: '1000kb'}))
 const configDir = './config-store'
 fs.ensureDirSync(configDir)
 
-router.get('/:configId', (req, res, next) => {
+router.get('', (req, res, next) => {
+  res.status(200).send({
+    userPermissions: [
+      'readDescription',
+      'readConfig',
+      'writeConfig'
+    ]
+  })
+})
+
+router.get('/config', (req, res, next) => {
   try {
     const content = fs.readFileSync(path.join(configDir, req.params.configId + '.json'), 'utf-8')
-    content.userPermissions = ['writeConfig']
     res.status(200).send(JSON.parse(content))
   } catch (err) {
     res.status(200).send({})
   }
 })
 
-router.put('/:configId', (req, res, next) => {
+router.put('/config', (req, res, next) => {
   const content = JSON.stringify(req.body, null, 2)
   fs.writeFileSync(path.join(configDir, req.params.configId + '.json'), content)
   res.status(200).send(req.body)
